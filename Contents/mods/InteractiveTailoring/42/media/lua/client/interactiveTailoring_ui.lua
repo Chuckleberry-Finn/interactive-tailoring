5be6cec5f9eb76ed3d94dd4241d8987afee7f6e8
Chuckleberry Finn <6071161+Chuckleberry-Finn@users.noreply.github.com>
2025-07-02 18:09:04 -0400

    holes and textures

--- Contents/mods/InteractiveTailoring/42/media/lua/client/interactiveTailoring_ui.lua
+++ Contents/mods/InteractiveTailoring/42/media/lua/client/interactiveTailoring_ui.lua
@@ -32,25 +32,41 @@ function interactiveTailoringUI:render()
 
     local tbh = self:titleBarHeight()
 
-    ---grid
+    local holeCords = {}
+
+    ---fabric
     local gridY = ((self.padding+self.gridScale)*2)+tbh
     for _x=0, self.gridW-1 do
         for _y=0, self.gridH-1 do
-            self:drawRect(
+            self:drawTextureScaled(self.fabricTexture,
                     self.padding + (self.gridScale*_x), gridY + (_y*self.gridScale),
                     self.gridScale, self.gridScale,
                     self.clothing:getA(), self.clothing:getR(), self.clothing:getG(), self.clothing:getB())
         end
     end
+
+    ---holes
+    for _x=1, #self.holes do
+        for _y=1, #self.holes[_x] do
+            if self.holes[_x][_y] then
+                self:drawTextureScaled(getTexture("media/textures/hole.png"),
+                        self.padding + (self.gridScale*(_x-1)), gridY + ((_y-1)*self.gridScale),
+                        self.gridScale, self.gridScale,
+                        self.clothing:getA(), self.clothing:getR(), self.clothing:getG(), self.clothing:getB())
+            end
+        end
+    end
+
     self:drawRectBorder(self.padding-2, gridY-2, (self.gridW*self.gridScale)+4, (self.gridH*self.gridScale)+4, 0.9, 0.5, 0.5, 0.5)
 
     ---sidebar
     local sidebarX = (self.padding*2)+(self.gridW*self.gridScale)
     for _x=0, 3-1 do
         for _y=0, self.gridH-1 do
+
             self:drawRect(
-                    sidebarX + (self.gridScale*_x), gridY + (_y*self.gridScale),
-                    self.gridScale, self.gridScale,
+                    sidebarX + (self.gridScale*_x)+1, gridY + (_y*self.gridScale)+1,
+                    self.gridScale-2, self.gridScale-2,
                     0.3, 0.3, 0.3, 0.3)
         end
     end
@@ -126,10 +142,20 @@ function interactiveTailoringUI:new(player, clothing)
     o.activeItemA = 0.7
     o.inactiveItemA = 0.4
 
+    o.holes = {}
+    for _x=1, o.gridW do
+        o.holes[_x] = {}
+        for _y=1, o.gridH do
+            o.holes[_x][_y] = (ZombRand(100) <= 2)
+        end
+    end
+
     o.clothingUI = {}
     o.clothingUI.icon = clothing:getTex()
     o.clothingUI.iW = o.clothingUI.icon:getWidthOrig()
     o.clothingUI.iH = o.clothingUI.icon:getHeightOrig()
+
+    o.fabricTexture = getTexture("media/textures/fabric.png")
 
     o.borderColor = {r=0.4, g=0.4, b=0.4, a=1}
     o.backgroundColor = {r=0, g=0, b=0, a=0.8}
