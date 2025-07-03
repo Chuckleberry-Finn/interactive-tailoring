dcac3ab806a952795b51490bcb132abb48a26fd0
Chuckleberry Finn <6071161+Chuckleberry-Finn@users.noreply.github.com>
2025-07-03 15:58:32 -0400

    Update interactiveTailoring_garmentUI.lua

--- Contents/mods/InteractiveTailoring/42/media/lua/client/interactiveTailoring_garmentUI.lua
+++ Contents/mods/InteractiveTailoring/42/media/lua/client/interactiveTailoring_garmentUI.lua
@@ -1,11 +1,9 @@
 require "ISUI/ISInventoryPaneContextMenu.lua"
 require "interactiveTailoring_ui.lua"
 
-local original = ISInventoryPaneContextMenu.onInspectClothingUI
-
+--local original = ISInventoryPaneContextMenu.onInspectClothingUI
 function ISInventoryPaneContextMenu.onInspectClothingUI(player, clothing)
-    original(player, clothing)
-
+    --original(player, clothing)
     if luautils.haveToBeTransfered(player, clothing) then
         local action = ISInventoryTransferAction:new(player, clothing, clothing:getContainer(), player:getInventory())
         action:setOnComplete(interactiveTailoringUI.open, player, clothing)
