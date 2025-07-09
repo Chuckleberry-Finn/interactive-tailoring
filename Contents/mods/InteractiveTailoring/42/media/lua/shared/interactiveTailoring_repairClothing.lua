d21ff4a77b3e7dda3268dcc53019b8a8b49432bf
Chuckleberry Finn <6071161+Chuckleberry-Finn@users.noreply.github.com>
2025-07-09 11:28:11 -0400

    fix vanilla issue with actions regarding padding vs patches

--- Contents/mods/InteractiveTailoring/42/media/lua/shared/interactiveTailoring_repairClothing.lua
+++ Contents/mods/InteractiveTailoring/42/media/lua/shared/interactiveTailoring_repairClothing.lua
@@ -1,9 +1,8 @@
-local orig = ISRepairClothing.complete
-
+require "TimedActions/ISRepairClothing.lua"
+local origRepairClothingComplete = ISRepairClothing.complete
 function ISRepairClothing:complete()
-    local result = orig(self)
+    local result = origRepairClothingComplete(self)
     if result then
-
         local itModData = self.clothing:getModData().interactiveTailoring
         local mdAreas = itModData and itModData.areas
         local part = self.part and mdAreas and mdAreas[self.part:index()]
@@ -14,4 +13,22 @@ function ISRepairClothing:complete()
         end
     end
     return result
+end
+
+local origRepairClothingUpdate = ISRepairClothing.update
+function ISRepairClothing:update()
+    origRepairClothingUpdate(self)
+    local hole = self.clothing:getVisual():getHole(self.part) > 0 ---To fix bug in vanilla code: hole is not compared to 0/1.
+    local jobType = hole and getText("ContextMenu_PatchHole") or getText("ContextMenu_AddPadding")
+    ISGarmentUI.setBodyPartActionForPlayer(self.character, self.part, self, jobType, { })
+end
+
+
+require "TimedActions/ISRemovePatch.lua"
+local origRemovePatchUpdate = ISRepairClothing.update
+function ISRemovePatch:update()
+    origRemovePatchUpdate(self)
+    local hole = self.clothing:getVisual():getHole(self.part) > 0 ---There isn't even a distinction made in vanilla for this.
+    local jobType = hole and getText("ContextMenu_PatchHole") or getText("ContextMenu_AddPadding")
+    ISGarmentUI.setBodyPartActionForPlayer(self.character, self.part, self, jobType, { })
 end
